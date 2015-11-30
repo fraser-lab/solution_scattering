714564b1d48aa55c08305267c3bf3f7c53cdc979
bbarad <benjamin.barad@gmail.com>
2015-11-30 14:15:39 -0800

    Wrapper for parsing

--- parse.py
+++ parse.py
@@ -1,17 +1,28 @@
 """ This part of the package is for loading data of various types and then 
-making traces
+making traces.
 
 Benjamin Barad
 """
 from numpy import recarray, load
 from trace import Trace
 
+def parse(filename, mode="tpkl"):
+	"""Wrapper function for any loader functions that I may write besides 
+	tpkl. Just passes through to the appropriate place based on the `mode` 
+	variable."""
+	if mode == "tpkl":
+		return parse_tpkl(filename)
 
 def parse_tpkl(filename):
 	"""Loads tpkl files and generates a corresponding Trace object. Requires
 	table.py from the Anfinrud lab, which we will not distribute.
 	"""
-	from table import table
+	try:
+		from table import table
+	except ImportError:
+		print """You do not have the required code accessible to parse tpkl
+		files. Try using a different file format for input"""
+		raise
 	data = load(filename)
 	q = data['q']
 	S = data['S']
