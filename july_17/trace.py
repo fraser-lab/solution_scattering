b08bd479f39b87395666f7d3cf0308c2ff9cba8a
Alexander Wolff <awolff@otp588685ots.ucsf.edu>
2017-07-13 13:27:20 -0700

    fix trace.py, spaces vs tabs made me sad

--- july_17/trace.py
+++ july_17/trace.py
@@ -29,31 +29,31 @@ class Trace(object):
 		return (self.scaled_SA, self.scaled_sigSA)
 
 	def alg_scale(self, ref):
-	    """Scale by projection of a vector onto a reference vector and determining the magnitude difference."""
-	    SA_ref = ref.SA
-	    SA_var = self.SA
-	    q = ref.q
-	    q_SA_ref = SA_ref*q
-	    q_SA_var = SA_var*q
-	    top = np.dot(q_SA_var,q_SA_ref)
-	    bottom = np.dot(q_SA_var,q_SA_var)
-	    scalar = top/bottom
-	    print("scalar: {}".format(scalar))
-	    SA_adjusted = SA_var * scalar
-	    sig_SA_adjusted = self.sigSA * scalar
-	    return SA_adjusted, sig_SA_adjusted
+		"""Scale by projection of a vector onto a reference vector and determining the magnitude difference."""
+		SA_ref = ref.SA
+		SA_var = self.SA
+		q = ref.q
+		q_SA_ref = SA_ref*q
+		q_SA_var = SA_var*q
+		top = np.dot(q_SA_var,q_SA_ref)
+		bottom = np.dot(q_SA_var,q_SA_var)
+		scalar = top/bottom
+		print("scalar: {}".format(scalar))
+		SA_adjusted = SA_var * scalar
+		sig_SA_adjusted = self.sigSA * scalar
+		return SA_adjusted, sig_SA_adjusted
 
-    def integration_scale(self, ref):
-	    """Scale by the total number of scattered photons"""
-	    SA_ref = ref.SA
-	    SA_var = self.SA
-	    q = self.q
-	    top = np.dot(SA_ref, q)
-	    bottom =  np.dot(SA_var, q)
-	    scalar = top/bottom
-	    SA_adjusted = SA_var * scalar
-	    sig_SA_adjusted = self.sigSA * scalar
-	    return SA_adjusted, sig_SA_adjusted
+	def integration_scale(self, ref):
+		"""Scale by the total number of scattered photons"""
+		SA_ref = ref.SA
+		SA_var = self.SA
+		q = self.q
+		top = np.dot(SA_ref, q)
+		bottom =  np.dot(SA_var, q)
+		scalar = top/bottom
+		SA_adjusted = SA_var * scalar
+		sig_SA_adjusted = self.sigSA * scalar
+		return SA_adjusted, sig_SA_adjusted
 
 	def as_vector(self):
 		""" The SA column is the air-scattering adjusted integrated intensity"""
