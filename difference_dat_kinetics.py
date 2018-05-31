1a65f876d761b2ede07ffe064410607473d32b28
Benjamin Barad <benjamin.barad@gmail.com>
2018-05-31 16:57:34 -0700

    invert area under curves instead of inverting plotting - we want the area over the curve anyway

--- difference_dat_kinetics.py
+++ difference_dat_kinetics.py
@@ -9,8 +9,8 @@ import trace
 
 PREFIX = "_data/CypA-H-1_diff_"
 TIMES_STR = ["-10.1us", "562ns", "750ns", "1us", "1.33us", "1.78us", "2.37us", "3.16us", "4.22us", "5.62us", "7.5us", "10us", "13.3us", "17.8us","23.7us", "31.6us", "42.2us", "56.2us", "75us", "100us", "133us", "178us", "237us", "316us", "422us", "562us", "750us", "1ms"]
-INITIAL_GUESS = (0, 1./1000, 0, 1./10000, 0) # (-1, 1./10000, 1)
-RELAXATION_STEPCOUNT = two_step_relaxation #single_step_relaxation 
+INITIAL_GUESS = (800,0, 200) #(0, 1./1000, 0, 1./10000, 0) # (-1, 1./10000, 1)
+RELAXATION_STEPCOUNT = single_step_relaxation #two_step_relaxation #single_step_relaxation 
 
 def time_str_to_float(time_string):
   number = float(time_string[:-2])
@@ -30,8 +30,8 @@ def plot_integrated_areas(tuple_list, y_calc, filename = "integrated_area_over_t
 	# Tuple list should be of the form [(time_numeric, trace, integrated area, integrated error),(),...]
 	fig, ax = plt.subplots()
 	x, _, y, yerr = zip(*tuple_list)
-	curve = ax.errorbar(x[1:],[-i for i in y[1:]], fmt=".", yerr=yerr[1:])
-	curve_2 = ax.plot(x, [-i for i in y_calc], '-')
+	curve = ax.errorbar(x[1:],y[1:], fmt=".", yerr=yerr[1:])
+	curve_2 = ax.plot(x, y_calc, '-')
 	ax.set_xscale('log')
 	ax.set_xlim(x[1], x[-1])
 	fig.savefig(filename)
@@ -58,11 +58,12 @@ def integrate_area(trace, q_min = 0.03, q_max = 0.06):
 	series_error = []
 	for i in range(index_low, index_high+1):
 		delta_q = q[i+1] - q[i]
+		print(delta_q)
 		I_section = trace.SA[i] * delta_q
 		error_section = trace.sigSA[i] * delta_q
 		series_I.append(I_section)
 		series_error.append(error_section)
-	integrated_area = sum(series_I)
+	integrated_area = -1*sum(series_I)
 	integrated_error = np.sqrt(sum([i**2 for i in series_error]))
 	return integrated_area, integrated_error
 
@@ -84,7 +85,10 @@ def run(prefix, times_str):
 	times,_,areas, errors = zip(*traces)
 	parameters, covariances, y_calc = measure_kinetics(areas, times, initial=INITIAL_GUESS, funct=RELAXATION_STEPCOUNT)
 	print("Parameters of Fit:")
-	print("First Step: \nA1 = {0}\nkobs1 = {1}\nSecond Step:\nA2 = {2}\nkobs2 = {3}\nOffset = {4}".format(*parameters)) # only works for 2-step! Comment out otherwise.
+	try:
+		print("First Step: \nA1 = {0}\nkobs1 = {1}\nSecond Step:\nA2 = {2}\nkobs2 = {3}\nOffset = {4}".format(*parameters)) # only works for 2-step! Comment out otherwise.
+	except:
+		print(parameters)
 	plot_integrated_areas(traces, y_calc)
 	plot_differences(traces)
 
