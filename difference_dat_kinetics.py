23982273ed255593af4b9e58a305517cabc6145d
Benjamin Barad <benjamin.barad@gmail.com>
2018-05-31 17:31:08 -0700

    Added sigma values to relax.py and difference_dat_kinetics.py

--- difference_dat_kinetics.py
+++ difference_dat_kinetics.py
@@ -7,10 +7,11 @@ from relax import relaxation_fit, single_step_relaxation, two_step_relaxation
 from parse import parse
 import trace
 
-PREFIX = "_data/CypA-H-1_diff_"
-TIMES_STR = ["-10.1us", "562ns", "750ns", "1us", "1.33us", "1.78us", "2.37us", "3.16us", "4.22us", "5.62us", "7.5us", "10us", "13.3us", "17.8us","23.7us", "31.6us", "42.2us", "56.2us", "75us", "100us", "133us", "178us", "237us", "316us", "422us", "562us", "750us", "1ms"]
-INITIAL_GUESS = (800,0, 200) #(0, 1./1000, 0, 1./10000, 0) # (-1, 1./10000, 1)
-RELAXATION_STEPCOUNT = single_step_relaxation #two_step_relaxation #single_step_relaxation 
+# PREFIX = "time_resolved_integration_scaling/nov_2016_cypa_1/CypA-1_diff_"
+PREFIX = "_data/"
+TIMES_STR = ["-10.1us", "562ns","750ns", "1us", "1.33us", "1.78us", "2.37us", "3.16us", "4.22us", "5.62us", "7.5us", "10us", "13.3us", "17.8us","23.7us", "31.6us", "42.2us", "56.2us", "75us", "100us", "133us", "178us", "237us", "316us", "422us", "562us", "750us", "1ms"]
+INITIAL_GUESS = (-10, 1./1000, 1, 1./10000, 3) # (-1, 1./10000, 1)(.8,1./10000, 2) #
+RELAXATION_STEPCOUNT = two_step_relaxation #single_step_relaxation 
 
 def time_str_to_float(time_string):
   number = float(time_string[:-2])
@@ -31,7 +32,7 @@ def plot_integrated_areas(tuple_list, y_calc, filename = "integrated_area_over_t
 	fig, ax = plt.subplots()
 	x, _, y, yerr = zip(*tuple_list)
 	curve = ax.errorbar(x[1:],y[1:], fmt=".", yerr=yerr[1:])
-	curve_2 = ax.plot(x, y_calc, '-')
+	curve_2 = ax.plot(x[1:], y_calc, '-')
 	ax.set_xscale('log')
 	ax.set_xlim(x[1], x[-1])
 	fig.savefig(filename)
@@ -68,11 +69,11 @@ def integrate_area(trace, q_min = 0.03, q_max = 0.06):
 	return integrated_area, integrated_error
 
 
-def measure_kinetics(area_series, time_series, initial, funct=two_step_relaxation, maxfev=30000):
+def measure_kinetics(area_series, time_series, initial, funct=two_step_relaxation, maxfev=30000, sigma=None):
 	x = time_series
 	y = area_series
-	popt, pcov, y_calc = relaxation_fit(x,y, relaxation_function=funct, initial_guess=initial, maxfev=maxfev)
-	return popt,pcov, y_calc
+	popt, pcov, y_calc = relaxation_fit(x,y, relaxation_function=funct, initial_guess=initial, maxfev=maxfev, sigma=sigma)
+	return popt, pcov, y_calc
 
 def run(prefix, times_str):
 	traces = []
@@ -83,11 +84,16 @@ def run(prefix, times_str):
 		area, error = integrate_area(trace)
 		traces.append((time_numeric,trace, area, error))
 	times,_,areas, errors = zip(*traces)
-	parameters, covariances, y_calc = measure_kinetics(areas, times, initial=INITIAL_GUESS, funct=RELAXATION_STEPCOUNT)
+	parameters, covariances, y_calc = measure_kinetics(areas[1:], times[1:], initial=INITIAL_GUESS, funct=RELAXATION_STEPCOUNT, sigma=errors[1:])
 	print("Parameters of Fit:")
-	try:
-		print("First Step: \nA1 = {0}\nkobs1 = {1}\nSecond Step:\nA2 = {2}\nkobs2 = {3}\nOffset = {4}".format(*parameters)) # only works for 2-step! Comment out otherwise.
-	except:
+	if RELAXATION_STEPCOUNT = two_step_relaxation:
+		print("First Step:")
+		print ("A1: {}\tVariance: {}".format(parameters[0], covariances[0][0]))
+		print ("kobs1: {}\tVariance: {}".format(parameters[1], covariances[1][1])) 
+		print ("A2: {}\tVariance: {}".format(parameters[2], covariances[2][2])) 
+		print ("kobs2: {}\tVariance: {}".format(parameters[3], covariances[3][3])) 
+		print ("offset: {}\tVariance: {}".format(parameters[3], covariances[3][3])) 
+	else:
 		print(parameters)
 	plot_integrated_areas(traces, y_calc)
 	plot_differences(traces)
