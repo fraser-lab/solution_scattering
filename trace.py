45aae1defc8dd3f671bf28329f4443957aeb84ff
Alexander Wolff <lifehasorder@gmail.com>
2019-06-14 14:53:04 -0700

    add docstrings to trace.py

--- trace.py
+++ trace.py
@@ -1,8 +1,11 @@
-"""The 'trace' class is to work with traces from SAXS/WAXS data. 
+"""
+The 'trace' class is to work with traces from SAXS/WAXS data. 
 It takes q and I or S and/or SA and provides simple utility classes 
 for understanding trace data.
 
-Benjamin Barad
+Author(s):
+Benjamin A. Barad
+Alexander M. Wolff
 """
 
 import numpy as np
@@ -19,12 +22,13 @@ class Trace(object):
 
 	def scale(self, ref, qmin=0.0025, qmax=5.1925, approach="algebraic"):
 		"""
-                Scale by the total number of scattered photons
+                Scale against a reference vector
 		
 		:param ref: Reference curve (other Trace object)
-        :param qmin: Minimum q for scaling (default 0.0025) 
+        	:param qmin: Minimum q for scaling (default 0.0025) 
 		:param qmax: Maximum q for scaling (default 5.1925)
-        :returns: Scaled SA and sigSA curves (which also get written on the trace as self.scaled_SA, self.scaled_sigSA)
+		:param approach: Algebraic (scattering angle-weighted), Projection (vector projection), or Integration (area under the curve)
+        	:returns: Scaled SA and sigSA curves (which also get written on the trace as self.scaled_SA, self.scaled_sigSA)
 		"""
 		SA_var = self.SA[self.q>=qmin]
 		SA_var = SA_var[self.q<=qmax] ###0.0025 works for full q across all cypa
@@ -64,7 +68,14 @@ class Trace(object):
 		return self.scaled_SA, self.scaled_sigSA
 
 	def buffer_scale(self, ref):
-		"""Scale by the total number of scattered photons"""
+		"""
+		Scale by the total number of scattered photons. Reference should
+		be matched buffer.
+		
+		:param ref: Buffer reference curve (other Trace object)
+		
+		:returns: NA, values stored as self.buffer_scale_factor, self.scaled_SA, and self.scaled_sigSA
+		"""
 		SA_ref = ref.SA
 		SA_var = self.SA
 		q = self.q
@@ -83,6 +94,18 @@ class Trace(object):
 		return
 
 	def subtract(self, trace_two, scaled=None, buffer_scaled=None):
+		"""
+		Subtract scattering (SA) of another trace object from the current object.
+		Measured errors are propogated.
+		
+		:param trace_two: trace to be subtracted (other Trace object)
+		:param scaled: Have both curves been scaled prior to using this method?
+		If so, scaled values will be subtracted. (default = None)
+		:param scaled: Have both curves been buffer-scaled prior to using this method?
+		If so, buffer-scaled values will be subtracted. (default = None)
+		
+		:returns: A new trace object that is the difference of the two curves
+		"""
 		if scaled:
 			err_one = (self.scale_factor*self.sigSA)**2
 			err_two = (trace_two.scale_factor*trace_two.sigSA)**2
@@ -105,6 +128,18 @@ class Trace(object):
 
 
 	def add(self, trace_two, scaled=None, buffer_scaled=None):
+		"""
+		Add scattering (SA) of another trace object to the current object's scattering (SA).
+		Measured errors are propogated.
+		
+		:param trace_two: trace to be added (other Trace object)
+		:param scaled: Have both curves been scaled prior to using this method?
+		If so, scaled values will be added. (default = None)
+		:param scaled: Have both curves been buffer-scaled prior to using this method?
+		If so, buffer-scaled values will be added. (default = None)
+		
+		:returns: A new trace object that is the sum of the two curves
+		"""
 		if scaled:
 			err_one = (self.scale_factor*self.sigSA)**2
 			err_two = (trace_two.scale_factor*trace_two.sigSA)**2
@@ -126,23 +161,35 @@ class Trace(object):
 		return output
 
 	def as_vector(self):
-		""" The SA column is the air-scattering adjusted integrated intensity"""
+		"""Returns air-scattering adjusted integrated intensity as a vector."""
 		return self.SA
 	
 	def get_q(self):
+		"""Returns scattering angle as a vector."""
 		return self.q
 
 	def set_name(self, name):
+		"""
+		Sets object's name.
+		
+		:param name: str to use as object's name
+		"""
 		self.name=name
 		return
 
 	def write_dat(self, filename):
+	    """
+	    Write trace as a text file with space-separated columns.
+		
+	    :param filename: str to use as filename
+	    """
 	    data = np.column_stack((self.q, self.SA, self.sigSA))
 	    np.savetxt(filename, data, fmt='%f', delimiter='    ', newline='\n', header='q    I    sigI')
 	    print("data successfully written to {}".format(filename))
 	    return
 		
 	def __repr__(self):
+		"""Return a printable version of the trace object.""""
 		final = "Q\tSA\tsigSA\tNj\n"
 		for index, _ in enumerate(self.q):
 			final += ("{0}\t{1}\t{2}\t{3}\n".format(self.q[index], self.SA[index],
