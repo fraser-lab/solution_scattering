859689a13a4bb330b5f837d287c6bcc86309af74
Alexander Wolff <lifehasorder@gmail.com>
2019-06-14 12:09:11 -0700

    add docstrings to parse.py

--- parse.py
+++ parse.py
@@ -1,7 +1,11 @@
-""" This part of the package is for loading data of various types and then 
-making traces.
+"""
+Parse provides a library of pre-built parse functions for different formats
+of solution scattering data. Functions are included which can convert data
+stored within the filename into python objects for downstream processing.
 
-Benjamin Barad
+Author(s):
+Benjamin A. Barad
+Alexander M. Wolff
 """
 from numpy import load
 from scipy import stats
@@ -14,9 +18,17 @@ import re
 
 
 def parse(filename):
-    """Wrapper function for any loader functions that I may write besides 
-    tpkl. Just passes through to the appropriate place based on the `mode` 
-    variable."""
+    """
+    A wrapper function which uses the suffix of the filename
+    to determine which method to call upon for parsing the file.
+    
+    Parameters:
+    filename (str): path of file to be analyzed
+    
+    Returns:
+    Trace:custom object built to hold a single scattering curve and
+    associated values
+    """
     if filename.endswith("tpkl"):
         return parse_tpkl_2(filename)
     elif filename.endswith("dat"):
@@ -25,6 +37,16 @@ def parse(filename):
         raise TypeError('scattering data can only be read from the following filetypes *.tpkl, *.dat')
 
 def parse_dat(filename):
+    """
+    A function to parse flat text files, with columns separated by spaces.
+    
+    Parameters:
+    filename (str): path of file to be analyzed
+    
+    Returns:
+    Trace:custom object built to hold a single scattering curve and
+    associated values
+    """
     data = read_table(filename, delimiter="    ", engine='python', skiprows=1, names=['q','I','sigI'])
     q = data.q
     SA = data.I
@@ -44,7 +66,15 @@ dt = np.dtype({'names': ['q','S','sigS','SA','sigSA','Nj'],
 
 
 def parse_tpkl_2(filename):
-    """Loads tpkl files and generates a corresponding Trace object.
+    """
+    A function to parse custom recarray objects.
+    
+    Parameters:
+    filename (str): path of file to be analyzed
+    
+    Returns:
+    Trace:custom object built to hold a single scattering curve and
+    associated values
     """
     TPKL_HEADER_BYTES = 279 ### this value could vary...original value
     # TPKL_HEADER_BYTES = 290 ### march 2018
@@ -55,7 +85,15 @@ def parse_tpkl_2(filename):
     return Trace(d2.q, d2.sigS, d2.S, d2.sigSA, d2.SA, d2.Nj)
 
 def parse_tpkl(filename):
-    """Loads tpkl files and generates a corresponding Trace object.
+    """
+    A function to parse custom recarray objects.
+    
+    Parameters:
+    filename (str): path of file to be analyzed
+    
+    Returns:
+    Trace:custom object built to hold a single scattering curve and
+    associated values
     """
     TPKL_HEADER_BYTES = 279 ### this value could vary
     with open(filename, "rb") as f:
@@ -71,20 +109,38 @@ def parse_tpkl(filename):
 
 
 def parse_tpkl_depreciated(filename):
-	"""Loads tpkl files and generates a corresponding Trace object.
-    This variation is dependent upon table.py from the Anfinrud lab
-    because the tpkl files are custom recarray objects"""
-	data = load(filename)
-	q = data.q
-	sigS = data.sigS
-	S = data.S
-	sigSA = data.sigSA
-	SA = data.SA
-	Nj = data.Nj
-	return Trace(q, sigS, S, sigSA, SA, Nj)
+    """
+    A function to parse custom recarray objects.
+    This variation is dependent upon table.py from the Anfinrud Lab
+    
+    Parameters:
+    filename (str): path of file to be analyzed
+    
+    Returns:
+    Trace:custom object built to hold a single scattering curve and
+    associated values
+    """
+    data = load(filename)
+    q = data.q
+    sigS = data.sigS
+    S = data.S
+    sigSA = data.sigSA
+    SA = data.SA
+    Nj = data.Nj
+    return Trace(q, sigS, S, sigSA, SA, Nj)
 
 
 def unit_sort(text):
+    """
+    A function to sort files when timepoints are encoded within the filename
+    using common abbreviations.
+    
+    Parameters:
+    text (str): filename e.g. (proteinX_100ns)
+    
+    Returns:
+    Tuple(int, text): for sorting files
+    """
     if text.startswith("-"):
         return 0, text
 
@@ -98,6 +154,17 @@ def unit_sort(text):
         return 3, text
 
 def atof(text):
+    """
+    Function to test whether a str can be rendered as a float.
+    Useful when retreiving experimental parameters that were
+    stored within the filename.
+    
+    Parameters:
+    text (str)
+    
+    Returns:
+    float or text
+    """
     try:
         retval = float(text)
     except ValueError:
@@ -106,27 +173,58 @@ def atof(text):
 
 def natural_keys(text):
     '''
-    alist.sort(key=natural_keys) sorts in human order
-    http://nedbatchelder.com/blog/200712/human_sorting.html
-    (See Toothy's implementation in the comments)
-    float regex comes from https://stackoverflow.com/a/12643073/190597
+    A regex function to help with sorting strings containing numeric and non-numeric chunks.
+    
+    Parameters:
+    text (str): e.g. if iterated over this list of strings [a1, a11, a2, a13]
+    
+    Returns:
+    int: e.g. would return a list with ints assigning sort position [1, 3, 2, 4]
     '''
     return [ atof(c) for c in re.split(r'[+-]?([0-9]+(?:[.][0-9]*)?|[.][0-9]+)', text) ]
 
 def tryint(s):
+    """
+    Function to test whether a str can be rendered as a int.
+    Useful when retreiving experimental parameters that were
+    stored within the filename.
+    
+    Parameters:
+    text (str)
+    
+    Returns:
+    int or text
+    """
     try:
         return int(s)
     except:
         return s
      
 def alphanum_key(s):
-    """ Turn a string into a list of string and number chunks.
-        "z23a" -> ["z", 23, "a"]
+    """ 
+    Turn a string into a list of string and number chunks.
+    "z23a" -> ["z", 23, "a"]
+    
+    Parameters:
+    text (str)
+    
+    Returns:
+    list of str and int values
     """
     return [ tryint(c) for c in re.split('([0-9]+)', s) ] 
 
 
 def times_numeric(text):
+    """
+    A function to convert timepoints encoded within the filename
+    into a float corresponding to the value in nanoseconds.
+    
+    Parameters:
+    text (str): e.g. (100us)
+    
+    Returns:
+    float: e.g. (100,000)
+    """
     number = float(text[:-2])
     if text.endswith("ns"):
         return number
